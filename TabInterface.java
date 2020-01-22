67941d16fd624dd957114c62b87fad74fc6fb8e4
Javier <mscjaviergs@gmail.com>
2020-01-22 00:33:16 -0700

    Update TabInterface.java

--- TabInterface.java
+++ TabInterface.java
@@ -1,18 +1,13 @@
 
 /**
  * Interface to be implemented by all name panels
- * @author: Sarthak Tiwari
- * @since:  1/22/2019
+ * @author: Anonymous
 */
 public interface TabInterface {
     
-    /** starts the timer thread on the tab */
-    public void startSayingHi();
-
-    /** stops the timer thread on the tab */
-    public void stopSayingHi();
-
-    /** returns the name to be shown on the tab */
+    /** 
+     * returns the name to be shown on the tab 
+     */
     public String getName();
 
 }
