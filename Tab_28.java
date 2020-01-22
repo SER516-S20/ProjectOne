1d747aad9a12a35597b7c65c632433b35fb474e6
Abhinaw Sarang <asarang@asu.edu>
2020-01-22 11:40:01 -0700

    Created Tab_28

---
+++ Tab_28.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Tab_28 extends JPanel implements TabInterface {
+
+  //The gitIds of the students involved
+  private String[] panelNumbers = {
+      "1", "2", "3", "4",
+      "5", "6", "7", "8",
+      "9", "10", "11", "12"
+  };
+
+  private JPanel[] panels = new JPanel[12];
+
+  public Tab_28() {
+
+    setLayout(new GridLayout(3, 4));
+    for (int i = 0; i < panelNumbers.length; i++) {
+      try {
+        Class<?> panelClass = Class.forName("Panel_" + panelNumbers[i]);
+        JPanel panel = (JPanel) panelClass.getDeclaredConstructor().newInstance();
+        panels[i] = panel;
+        add(panel);
+      } catch (Exception e) {
+        addError(panelNumbers[i]);
+      }
+    }
+  }
+
+  private void addError(String panelNumber) {
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel("Panel " + panelNumber + " Error");
+    panel.add(label);
+    add(panel);
+  }
+
+  @Override
+  public String getName() {
+    return "Abhinaw";
+  }
+
+}
