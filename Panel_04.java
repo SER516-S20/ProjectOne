72f403dbff5b71c337efec19b8ba206d3efc4d40
surya-de <58053679+surya-de@users.noreply.github.com>
2020-01-23 15:41:07 -0700

    Updated student ID

--- Panel_04.java
+++ Panel_04.java
@@ -4,10 +4,10 @@ import java.awt.*;
  * @author: Suryadeep
  * @date:23rd Jan 2020 
  */
-public class Panel_4 extends PanelInterface {
+public class Panel_04 extends PanelInterface {
 
   private JLabel label;
-  private String myName = " 4 Suryadeep";
+  private String myName = " 04 Suryadeep";
 
   public Panel_4() {
     super ("Suryadeep");
