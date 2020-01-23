f73102d474ae9e6e795f772851d8420f861c052c
praveen kumar panku <praveenkumarpanku@gmail.com>
2020-01-23 08:30:49 -0700

    Added Panel_25

---
+++ Panel_25.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_25 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Praveen Kumar";
+
+  public Panel_25() {
+    super ("Praveen Kumar");
+  }
+
+}
