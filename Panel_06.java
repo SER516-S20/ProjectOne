7f1f5e796b788ec1cd0c47f4094a88271dab8241
Ashutosh Dey <adey13@asu.edu>
2020-01-22 19:53:24 -0700

    Added Panel_06

---
+++ Panel_06.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/*******************
+ created by Ashutosh Dey
+ ******************/
+
+public class Panel_06 extends PanelInterface {
+    private JLabel label;
+    private String myName="Ashutosh Dey";
+    public Panel_06() {
+        super("Ashutosh Dey");
+    }
+}
