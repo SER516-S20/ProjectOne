ee798314ddce893639612d0e61f88a56f6d5cbe3
Ashwath Reddy <rashwath55@gmail.com>
2020-01-23 13:10:35 -0700

    added panel 18

---
+++ Panel_18.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_18 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Ashwath Reddy Koppala";
+
+  public Panel_18() {
+    super ("Ashwath");
+  }
+
+}
