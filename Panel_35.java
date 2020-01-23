b5c656a4ae60985ad2fa68f70ace53ff5095237e
Srinivasan Sundar <srini@Srinivasans-MacBook-Pro.local>
2020-01-22 19:55:15 -0700

    Student ID - 35 created panel 35

---
+++ Panel_35.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author srinivasan sundar
+ * @date 01/22/2020
+ */
+public class Panel_35 extends PanelInterface {
+
+    public Panel_35() {
+        super ("Srinivasan Sundar");
+    }
+
+}
