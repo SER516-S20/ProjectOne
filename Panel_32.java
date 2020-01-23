b6c4719a004ae83ca37aa76ec2d9edb2a60bee72
Rohit <r4rohit002@gmail.com>
2020-01-23 21:38:47 +0530

    Added Panel for ID 32

---
+++ Panel_32.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_32 {
+
+	/*
+	* @author Rohit Kumar Singh
+	* @Created on - 23 January 2020
+	*/
+	
+	Panel_32()
+	{
+		super("Rohit Kumar Singh");
+	}
+	
+}
