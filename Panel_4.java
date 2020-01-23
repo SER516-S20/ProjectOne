0eea92d6681a8e0c9699be69faadf69a1a55009c
surya-de <58053679+surya-de@users.noreply.github.com>
2020-01-23 12:30:12 -0700

    Added Panel_4 for Suryadeep

---
+++ Panel_4.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+/*
+ * @author: Suryadeep
+ * @date:23rd Jan 2020 
+ */
+public class Panel_4 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = " 4 Suryadeep";
+
+  public Panel_4() {
+    super ("Suryadeep");
+  }
+
+}
