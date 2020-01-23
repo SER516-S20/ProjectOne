4ffb151e60976ccf8a23e5107902d4bc59637b87
nlakshm <54956085+nlakshm@users.noreply.github.com>
2020-01-23 12:18:48 -0700

    Added panel 19

---
+++ Panel_19.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+ * @author Nachiappan Lakshmanan
+ * @Created on - 23 January 2020
+ */
+public class Panel_19 extends PanelInterface {
+
+	private JLabel label;
+	private String myName = "Nachiappan Lakshmanan";
+
+	public Panel_19() {
+		super("Nachiappan Lakshmanan");
+	}
+
+}
