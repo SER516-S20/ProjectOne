bae3f4b138b90ef9633d70e94b8478f1c0309838
surya-de <58053679+surya-de@users.noreply.github.com>
2020-01-23 15:44:40 -0700

    Updating the student id for suryadeep

--- Panel_04.java
+++ Panel_04.java
@@ -9,7 +9,7 @@ public class Panel_04 extends PanelInterface {
   private JLabel label;
   private String myName = " 04 Suryadeep";
 
-  public Panel_4() {
+  public Panel_04() {
     super ("Suryadeep");
   }
 
