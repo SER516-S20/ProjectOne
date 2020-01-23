ab89bded47bebaff2a6edde05f002f19a2a3e097
r7598412 <r7598412@gmail.com>
2020-01-22 18:42:39 -0700

    Create Panel_03.java
    Add Panel_03.java

---
+++ Panel_03.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_03 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "ShihYu Chang";
+
+  public Panel_03() {
+    super ("ShihYu Chang");
+  }
+
+}
+ 
