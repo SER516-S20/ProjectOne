178671936b331bd5bdc7a363525c43e374df1b48
Sarvansh Prasher <sarvanshp@gmail.com>
2020-01-22 22:00:38 -0700

    Added panel for 26 id

---
+++ Panel_26.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/*
+ * @author -Sarvansh Prasher
+ * @Created on - 22 January 2020
+ */
+
+
+public class Panel_34 extends PanelInterface {
+
+    private JLabel label;
+    private String myName = "Sarvansh Prasher";
+
+    public Panel_34() {
+        super ("Sarvansh Prasher");
+    }
+
+}
