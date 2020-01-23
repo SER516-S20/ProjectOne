f1a871efdf41dfdba7abed6851e302264b3cd217
rishikabera <55071483+rishikabera@users.noreply.github.com>
2020-01-23 13:52:48 -0700

    Created Panel_02.java

---
+++ Panel_02.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+/*******************
+ Created by : Rishika Bera
+ ******************/
+public class Panel_02 extends PanelInterface {
+
+
+  public Panel_02() {
+    super ("Rishika Bera");
+  } 
+
+}
