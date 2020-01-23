589b8f6c1d0e0fe34d703e64f0d87d5545b8dd73
ksharm38 <54971135+ksharm38@users.noreply.github.com>
2020-01-23 12:34:40 -0700

    Added Panel 29 for Kunal

---
+++ Panel_29.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+ * @author - Kunal Sharma
+ 
+   * @Created on - 22 January 2020
+ */
+
+public class Panel_29 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = " 29 Kunal Sharma";
+
+  public Panel_29() {
+    super ("Kunal");
+  }
+
+}
