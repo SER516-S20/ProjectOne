5470b53978614c13715c069cacdd4e3b5f076896
Parikshith Kedilaya <pkedilay@asu.edu>
2020-01-23 12:09:49 -0700

    Adding Panel SID - 20

---
+++ Panel_20.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+*@author - Parikshith Kedilaya Mallar
+*@Created on - 23 January 2020
+*/
+
+
+public class Panel_20 extends PanelInterface {
+
+  public Panel_20() {
+    super ("Parikshith");
+  }
+
+}
