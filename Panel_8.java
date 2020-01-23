569f87290f6d8ebef44bea5307a3c7bbf65d7736
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-01-22 19:31:19 -0700

    # StudentID - 8, Sheran Dass Dominic Ravichandran

---
+++ Panel_8.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*******************
+ Name:Sheran Dass
+ ******************/
+
+public class Panel_8 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Sheran Dass";
+
+  public Panel_8() {
+    super ("Sheran Dass");
+  }
+
+}
