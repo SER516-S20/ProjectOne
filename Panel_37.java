acabf6d41f507a31b191ecdc145e365e0d57a5f1
creativebuilder <csaravindt@gmail.com>
2020-01-23 10:24:46 -0700

    Student ID-37 Aravind Thillai Villalan Added Panel_37

---
+++ Panel_37.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Aravind Thillai Villalan
+ * @version 1.0
+ * @since 01/23/2020
+ */
+
+public class Panel_37 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Aravind Thillai Villalan";
+
+  public Panel_37() {
+    super ("Aravind Thillai Villalan");
+  }
+
+}
