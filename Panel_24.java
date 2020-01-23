491f4c8297dcf582dc81058b6f4cd338ef42ca1c
Chanyadav <yadavchan23@gmail.com>
2020-01-23 12:12:33 -0700

    Updated Panel_24

--- Panel_24.java
+++ Panel_24.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel_24 extends PanelInterface {
+  
+  /*
+	* @author Chandan Yadav
+	* @Created on - 22 January 2020
+	*/
 
   private JLabel label;
   private String myName = "Chandan Yadav";
