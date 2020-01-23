9678242a04b93d8b76e3e5939a14a728d88a8eba
hongqi Zhang <hzhan193@asu.edu>
2020-01-22 23:16:43 -0700

    Create Panel_39.java
    create panel with student name

---
+++ Panel_39.java
@@ -0,0 +1,14 @@
+import javax.swing.JLabel;
+
+public class Panel_39 extends PanelInterface{
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel label;
+	private String myName = "Hongqi Zhang";
+
+	public Panel_39() {
+		super ("Hongqi Zhang");
+	}
+}
