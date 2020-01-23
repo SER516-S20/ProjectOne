9d95266a3926e257837dbcbca214fc1426fd2a84
marcushsu123 <marcushsu123@gmail.com>
2020-01-22 18:38:49 -0700

    Create Panel_12.java

---
+++ Panel_12.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+/*******************
+ Name:Kai-Rui Hsu
+ ******************/
+public class Panel_12 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Kai-Rui Hsu";
+
+  public Panel_12() {
+    super ("Kai-Rui Hsu");
+  }
+
+}
