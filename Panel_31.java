441f9460f84d35d2c139769de683b01a0fac00fa
anushasingh30 <anusha2429@gmail.com>
2020-01-23 12:49:39 -0700

    Add Panel_31

---
+++ Panel_31.java
@@ -0,0 +1,15 @@
+import java.swing.*;
+import java.awt.*;
+
+/**
+@auhor: Anusha Singh 
+*/
+
+public class Panel_31 extends PanelInterface
+{
+
+    public Panel_31()
+    {
+        super("Anusha Singh");
+    }
+}
