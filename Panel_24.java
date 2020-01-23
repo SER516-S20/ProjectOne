71065e872b21928ed2821710a23ec54ce1eb0ef5
Chanyadav <yadavchan23@gmail.com>
2020-01-22 21:23:02 -0700

    Changes for Panel_24.java

---
+++ Panel_24.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_24 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Chandan Yadav";
+
+  public Panel_24() {
+    super ("Chandan Yadav");
+  }
+
+}
