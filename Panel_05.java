110785828d288758e6c048934ea52f6acb210744
Nikitha-ch <nikithachilivery@gmail.com>
2020-01-23 12:51:13 -0700

    added fifth panel

---
+++ Panel_05.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_05 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Nikitha";
+
+  public Panel_05() {
+    super ("Nikitha");
+  }
+
+}
