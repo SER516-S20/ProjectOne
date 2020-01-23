2e8a643d8144b4cad07d205242efdae50bee2d81
adityab079 <36786132+adityab079@users.noreply.github.com>
2020-01-23 13:20:11 -0700

    Create Panel_01.java

---
+++ Panel_01.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_01 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Aditya Bajaj";
+
+  public Panel_01() {
+    super ("Aditya Bajaj");
+  }
+
+}
