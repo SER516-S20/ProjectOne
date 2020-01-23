2c0b230b7314bebf67e25bf524442e2057ec4652
chandankj399 <chandankj399@gmail.com>
2020-01-22 23:11:01 -0700

    Added Panel 15

--- Panel_15.java
+++ Panel_15.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 
+/*
+*@author - Chandan Kiragadalu Javaregowda
+*@Created on - 22 January 2020
+*/
+
+
 public class Panel_15 extends PanelInterface {
 
   public Panel_15() {
