f1714bef8c7a9e63d7d13b083a0b6062fe0a18cd
shalikar <60168233+shalikar@users.noreply.github.com>
2020-01-22 22:01:26 -0700

    Create Panel_11.java

---
+++ Panel_11.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_11 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Suyog Halikar";
+
+  public Panel_11() {
+    super ("Suyog");
+  }
+
+}
