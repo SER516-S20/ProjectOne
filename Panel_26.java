080a94bfc8cc1d31671ee3c2b9b7dd714e9763a2
Sarvansh Prasher <sarvanshp@gmail.com>
2020-01-22 22:02:32 -0700

    Added proper class name

--- Panel_26.java
+++ Panel_26.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
-
+import java.awt.*;
 /*
  * @author -Sarvansh Prasher
  * @Created on - 22 January 2020
  */
 
 
-public class Panel_34 extends PanelInterface {
+public class Panel_26 extends PanelInterface {
 
     private JLabel label;
     private String myName = "Sarvansh Prasher";
 
-    public Panel_34() {
+    public Panel_26() {
         super ("Sarvansh Prasher");
     }
 
