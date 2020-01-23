3a427d5df5b22b5edc4369cdfc9c5f763705869a
Ashwin <ashwinsrinivasan97@gmail.com>
2020-01-23 08:37:25 +0530

    Panel_34 Java File

---
+++ Panel_34.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/*******************
+ Name:Ashwin Srinivasan
+ ******************/
+
+public class Panel_34 extends PanelInterface {
+
+    private JLabel label;
+    private String myName = "Ashwin";
+
+    public Panel_34() {
+        super ("Ashwin");
+    }
+
+}
