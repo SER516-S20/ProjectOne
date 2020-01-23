37239d2cfec2755090f5632a7ca005466e25e1a0
Rohithvarma1998 <36322767+Rohithvarma1998@users.noreply.github.com>
2020-01-23 07:05:41 -0700

    Add files via upload

---
+++ Panel_09.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Panel_09 extends PanelInterface
+{
+	private JLabel label;
+	private String myName = "Rohith Varma Gaddam";
+	public Panel_09()
+	{
+		super("Rohith Varma");
+	}
+	
+}
