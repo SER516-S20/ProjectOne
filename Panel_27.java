91e0368e05119f2fdabd7546b938a8ff395dad75
Pradeep Relangi <45675844+PradeepRelangi@users.noreply.github.com>
2020-01-23 13:31:12 -0700

    Added panel 27

---
+++ Panel_27.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_27 extends PanelInterface {
+
+  private JLabel label;
+  private String myName = "Sree Pradeep Kumar Relangi";
+
+  public Panel_27() {
+    super ("Pradeep");
+  }
+
+}
