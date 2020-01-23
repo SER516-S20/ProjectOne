2c0b45efaa11390f0bbac766d2bb37df7e0d7442
san1197 <sandya7c@gmail.com>
2020-01-22 21:42:48 -0700

    Class

--- Panel_22.java
+++ Panel_22.java
@@ -6,5 +6,10 @@ import javax.swing.*;
 
 public class Panel_22 extends PanelInterface
 {
-	
+	private JLabel label;
+	private String myName="Sandya Manoharan";
+	public Panel_22()
+	{
+		super("Sandya Manoharan")
+	}
 }
