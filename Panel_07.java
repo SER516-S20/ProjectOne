0ac25d2b72626c84d0235fbb4d37e96fdf0d7f13
ddharne <ddharne@users.github.com>
2020-01-24 12:00:13 -0700

    Update Panel_07.java
    Updated the Panel_07

--- Panel_07.java
+++ Panel_07.java
@@ -6,6 +6,10 @@ import java.awt.*;
  Created by : Dhananjay Dharne
  ******************/
 public class Panel_07 extends PanelInterface {
+	
+  private JLabel label;
+  private String myName = "07 Dhananjay Dharne";
+  
   public Panel_07() {
     super ("Dhananjay Dharne");
   } 
