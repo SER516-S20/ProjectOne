2387a17688475a71a2aebe9c6d0513c52c7cfc0c
ddharne <ddharne@users.github.com>
2020-01-24 11:48:03 -0700

    Create Panel_07.java
    Added Panel_07

---
+++ Panel_07.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+/*******************
+ Created by : Dhananjay Dharne
+ ******************/
+public class Panel_07 extends PanelInterface {
+  public Panel_07() {
+    super ("Dhananjay Dharne");
+  } 
+
+}
